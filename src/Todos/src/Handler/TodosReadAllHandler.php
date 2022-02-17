13ce1f2309c0506246b1420379fb11cb045246cd
tapiwa <tapiwakujinga@gmail.com>
2022-02-18 00:43:34 +0200

    Adds a feature to get all todos from the database

--- src/Todos/src/Handler/TodosReadAllHandler.php
+++ src/Todos/src/Handler/TodosReadAllHandler.php
@@ -4,18 +4,38 @@ declare(strict_types=1);
 
 namespace Todos\Handler;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Todos\Entity\Todo;
 
 class TodosReadAllHandler implements RequestHandlerInterface
 {
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Create and return a response
-        $response['status'] = 200;
-        $response['message'] = 'Hello World.';
+        $repository = $this->entityManager->getRepository(Todo::class);
+        $query = $repository->createQueryBuilder('t')->getQuery();
+        $paginator = new Paginator($query);
+        $entities = $paginator->getQuery()->getResult(Query::HYDRATE_ARRAY);
+
+        $response = [];
+
+        foreach ($entities as $index => $entity) {
+            $entity['isComplete'] = $entity['isComplete'] ? true : false;
+            $response[$index] = $entity;
+        }
+
         return new JsonResponse($response);
     }
 }
--- src/Todos/src/Handler/TodosReadAllHandlerFactory.php
+++ src/Todos/src/Handler/TodosReadAllHandlerFactory.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Todos\Handler;
 
+use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
 
 class TodosReadAllHandlerFactory
 {
     public function __invoke(ContainerInterface $container) : TodosReadAllHandler
     {
-        return new TodosReadAllHandler();
+        $entityManager = $container->get(EntityManager::class);
+        return new TodosReadAllHandler($entityManager);
     }
 }
